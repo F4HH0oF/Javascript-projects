108bdcf9acf36ab18fc833c3874c3e0d1901ad9b
Alekzander Ignatov <91214228+F4HH0oF@users.noreply.github.com>
2022-10-09 21:51:42 +0300

    Add files via upload

--- Modal Window/main.js
+++ Modal Window/main.js
@@ -3,8 +3,24 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const bntCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+const btnsOpenModal = document.querySelectorAll('.show-modal'); //Smth like an array
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+bntCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
